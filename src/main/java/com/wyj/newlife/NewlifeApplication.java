d8bb7feacfbb4f856dd5cc52570ee7438b20162a
yafeites <yafeites@DESKTOP-I31UEK4>
2021-05-04 20:24:33 +0800

    Initial commit

---
+++ src/main/java/com/wyj/newlife/NewlifeApplication.java
@@ -0,0 +1,13 @@
+package com.wyj.newlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewlifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewlifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyj/newlife/NewlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyj.newlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewlifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
